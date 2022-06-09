1681724c30a3c6a832330bc0581be402866a6fd2
Vodinov <igorvodinov@gmail.com>
2022-06-09 21:00:22 +0300

    added Junit & Parameterized

---
+++ src/test/java/com/gmail/igorvodinov/SimpleTest2.java
@@ -0,0 +1,20 @@
+package com.gmail.igorvodinov;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Класс с тестами проверяющими 3 и 2")
+public class SimpleTest2 {
+    @Test
+    @DisplayName("Тест проверяет, что 3 < 2")
+    void test() {
+        Assertions.assertTrue(3 < 2);
+    }
+
+    @Test
+    @DisplayName("Тест проверяет, что 3 > 2")
+    void test1() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
